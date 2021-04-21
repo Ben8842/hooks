5bb217ddd730364ad3c1d5350a1cb56ddadb4d4d
Ben8842 <z87e99b44r55a@gmail.com>
2021-04-21 14:32:19 -0400

    more

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Timed from "./components/Timed.js";
 
 function App() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [count, setCount, count2, setCount2] = useState(0);
 
   return (
     <div>
+      <p>You have {count} points</p>
       <p>You have {count} points</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
       <button onClick={() => setCount(count + 2)}>Click me</button>
@@ -16,7 +17,12 @@ function App() {
       <button onClick={() => setCount(count * 2)}>Click me</button>
       <button onClick={() => setCount(count * 3)}>Click me</button>
       <button onClick={() => setCount(count * 6)}>Click me</button>
-      <Timed />
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(count + 2)}>Click me</button>
+      <button onClick={() => setCount(count + 3)}>Click me</button>
+      <button onClick={() => setCount(count * 2)}>Click me</button>
+      <button onClick={() => setCount(count * 3)}>Click me</button>
+      <button onClick={() => setCount(count * 6)}>Click me</button>
     </div>
   );
 }
--- src/components/Timed.js
+++ src/components/Timed.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 
 function Timed() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  function bounceUp() {
+    setCount(bounce + 1);
+  }
+  var { interval: interval } = this.state;
+  const [bounce, setCount] = useState(0);
+  var interval = setInterval(bounceUp.bind(this), 25);
 
   return (
     <div>
       <div>Hello World</div>
+      <div>{bounce} is the bounce</div>
     </div>
   );
 }
